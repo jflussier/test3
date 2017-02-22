c61881e9ff9908d23e7bce7cff118cb2b2872a18
Jean-Francois Lussier <Jean-Francois.Lussier@centris.ca>
2017-02-22 16:23:50 -0500

    blablabla ddddee

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            string test = "blabla";
+            string test = "blablaqq";
             return View();
         }
 
